452ab1def145ea90f11f1d8956f12c8aa7d2992f
manoshiramanayake <54468424+manoshiramanayake@users.noreply.github.com>
2019-10-08 20:03:17 +0530

    Update JavaConnect.java

--- src/JavaConnect.java
+++ src/JavaConnect.java
@@ -7,6 +7,7 @@ public class JavaConnect {
     public static Connection ConnectorDb() {
         try{
             Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:E:/KDU/SE Project Docs/KDU Southern Library Management System/db/lms.db");
             return conn;
         }catch(Exception e){
          JOptionPane.showMessageDialog(null, e);
